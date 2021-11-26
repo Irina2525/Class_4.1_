239eb5b78c032161111407f8cb1c71f5df4cf76a
Irina <spartak.irina92@gmail.com>
2021-11-26 18:25:04 +0700

    Добавьте файлы проекта.

---
+++ Class_4.1_/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_4._1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число N > 0");
+            int N, sqr;
+            N = Convert.ToInt32(Console.ReadLine());
+            sqr = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                if
+                 (i > N)
+                    break;
+                Console.WriteLine("Квадрат числа {0} = {1} ", i, sqr += i * 2 - 1);
+            }
+            Console.ReadKey();
+        }
+    }
+}
